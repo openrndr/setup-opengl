020a94a5a72ca2feb2ed965ab24bac96d17bda0f
Edwin Jakobs <edwin@rndr.studio>
2020-11-09 19:02:14 +0100

    Another attempt at fixing environment variables

--- setup-opengl.sh
+++ setup-opengl.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 pwd
 tar -zxf $PAYLOAD_PATH/mesa-18.2.3.tar.gz
-echo $LD_LIBRARY_PATH
-echo $GALLIUM_DRIVER
+
 #export LD_LIBRARY_PATH=mesa-18.2.3/usr/local/lib/x86_64-linux-gnu
 #export GALLIUM_DRIVER=swr
+echo "LD_LIBRARY_PATH=mesa-18.2.3/usr/local/lib/x86_64-linux-gnu" >> $GITHUB_ENV
+echo "GALLIUM_DRIVER=swr" >> $GITHUB_ENV
+
 sudo apt-get install mesa-utils
 sudo apt-get install xvfb
-xvfb-run glxinfo
+LD_LIBRARY_PATH=mesa-18.2.3/usr/local/lib/x86_64-linux-gnu GALLIUM_DRIVER=swr xvfb-run glxinfo
