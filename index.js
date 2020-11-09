b3e87e972848c676125bd906341fa54e687f2f03
Edwin Jakobs <edwin@rndr.studio>
2020-11-09 18:43:03 +0100

    Attempt at removing set-env

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ try {
             'PAYLOAD_PATH' : __dirname
         }
     }
-    exec(path.join(__dirname, "setup-opengl.sh"), [], options)
     core.exportVariable('LD_LIBRARY_PATH', 'mesa-18.2.3/usr/local/lib/x86_64-linux-gnu')
     core.exportVariable('GALLIUM_DRIVER', 'swr')
+    exec(path.join(__dirname, "setup-opengl.sh"), [], options)
+
 } catch(error) {
     core.setFailed(error.message)
 }
